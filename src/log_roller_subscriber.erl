7f4dd2a47d683a36ac93e07b7550b513316c9af8
Jacob Vorreuter <jvorreuter@ea.com>
2009-02-18 18:23:13 -0800

    stripping out mnesia code

--- src/log_roller_subscriber.erl
+++ src/log_roller_subscriber.erl
@@ -29,59 +29,45 @@
 
 -export([subscribe_to/1, fetch/0, fetch/1, unique_nodes/0]).
 
--include_lib("stdlib/include/qlc.hrl").
 -include("log_roller.hrl").
 
 subscribe_to(Node) when is_list(Node) -> subscribe_to(list_to_atom(Node));
 subscribe_to(Node) when is_atom(Node) ->
 	gen_server:call(?MODULE, {subscribe_to, Node}).
 	
+%% =============================================================================
+%% API FUNCTIONS
+%% =============================================================================
 fetch() -> fetch([]).
 	
 fetch(Opts) when is_list(Opts) ->
-	match_objects(Opts).
+	gen_server:call(?MODULE, {fetch, Opts}).
+
+%% =============================================================================
+%% GEN_SERVER CALLBACKS
+%% =============================================================================
 
 start() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init(_) ->
-	ok = db_init(),
     {ok, []}.
 
-db_init() ->
-	case (catch mnesia:create_schema([node()])) of
-		ok -> 
-			ok = mnesia:start(),
-		    ok = mnesia:wait_for_tables(mnesia:system_info(local_tables), infinity),
-			ok = create_counter_table(),
-			ok = create_log_table();
-		{error,{_,{already_exists,_}}} -> 
-			ok = mnesia:start(),
-		    ok = mnesia:wait_for_tables(mnesia:system_info(local_tables), infinity),
-			ok = create_counter_table(),
-			ok = create_log_table()
-	end, ok.
-
 handle_call({subscribe_to, Node}, _From, State) ->
-	ok = gen_event:call({error_logger, Node}, log_roller_h, {subscribe, self()}),
-	{reply, ok, State};
-	
+	Res = gen_event:call({error_logger, Node}, log_roller_h, {subscribe, self()}),
+	{reply, Res, State};
+
+handle_call({fetch, Opts}, _From, State) ->
+	{reply, [], State};
+		
 handle_call(_, _From, State) -> {reply, {error, invalid_call}, State}.
 
 handle_cast(_Message, State) -> {noreply, State}.
 
 handle_info({log_roller, _Sender, Log}, State) ->
 	io:format("received a log: ~p~n", [Log]),
-	case (catch mnesia:dirty_update_counter(log_roller_counter, log_entry, 1)) of
-		NextID when is_integer(NextID) ->
-			case mnesia:dirty_write(log_roller_logs, Log#log_entry{id=NextID}) of
-				ok -> ok;
-				Err -> io:format("log write failed: ~p~n", [Err])
-			end;
-		Err ->
-			io:format("log write failed: ~p~n", [Err])
-	end,
-	{noreply, State};
+	{ok, State1} = write_log(State, Log),
+	{noreply, State1};
 
 handle_info(_Info, State) -> {noreply, State}.
 
@@ -89,94 +75,12 @@ terminate(_Reason, _State) -> ok.
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 	
-%% internal
+%% =============================================================================
+%% INTERNAL FUNCTIONS
+%% =============================================================================
 
-create_counter_table() ->
-	case (catch mnesia:create_table(log_roller_counter, 
-			[{disc_copies, [node()]}, 
-			{attributes, record_info(fields, log_roller_counter)}])) of
-		{atomic, ok} -> 
-			ok = mnesia:wait_for_tables(mnesia:system_info(local_tables), infinity),
-			ok;
-		{aborted, Err} -> 
-			io:format("counter exists: ~p~n", [Err]), 
-			ok
-	end.
-
-create_log_table() ->
-	case (catch mnesia:create_table(log_roller_logs, 
-			[{disc_copies, [node()]}, 
-			{type, ordered_set},
-		    {record_name, log_entry},
-			{attributes, record_info(fields, log_entry)}])) of
-		{atomic, ok} -> 
-			ok = mnesia:wait_for_tables(mnesia:system_info(local_tables), infinity),
-			ok;
-		{aborted, Err} -> 
-			io:format("log_roller_logs exists: ~p~n", [Err]), 
-			ok
-	end.
+write_log(State, Log) ->
 	
-unique_nodes() ->
-	QH = qlc:q([Node || {log_entry, _, _, Node, _, _} <- mnesia:table(log_roller_logs)], {unique, true}),
-	F = fun() -> qlc:eval(QH) end,
-	case mnesia:transaction(F) of
-		{atomic, Res} -> Res;
-		_ -> []
-	end.
-	
-match_objects(Opts) ->
-	Max = proplists:get_value(max, Opts, 100),
-	
-	Type_Fun = 
-		fun(Type0) -> 
-			case proplists:get_value(type, Opts, all) of
-				all -> 
-					true;
-				Type0 when is_atom(Type0) ->
-					true;
-				Types when is_list(Types) ->
-					lists:member(Type0, Types);
-				_ -> 
-					false
-			end
-		end,
-		
-	Node_Fun =
-		fun(Node0) ->
-			case proplists:get_value(node, Opts) of
-				undefined ->
-					true;
-				Node0 when is_list(Node0) ->
-					true;
-				_ ->
-					false
-			end
-		end,
-
-	QH = qlc:q([[ID, Type, Node, format_time(Time), Msg] 
-				 || {log_entry, ID, Type, Node, Time, Msg} 
-				  <- mnesia:table(log_roller_logs), Type_Fun(Type), Node_Fun(Node)
-			    ]),
-	F = fun() ->
-			QC = qlc:cursor(QH),
-			case (catch qlc:next_answers(QC, Max)) of
-				Res when is_list(Res) ->
-					qlc:delete_cursor(QC),
-					Res;
-				Err ->
-					qlc:delete_cursor(QC),
-					io:format("error fetching: ~p~n", [Err]),
-					[]
-			end
-		end,
-		
-	case mnesia:transaction(F) of
-		{atomic, Res} ->
-			Res;
-		Err ->
-			Err
-	end.
 
 format_time({{Y,Mo,D},{H,Mi,S}}) ->
 	lists:flatten(io_lib:format("~w-~2.2.0w-~2.2.0w ~2.2.0w:~2.2.0w:~2.2.0w", [Y,Mo,D,H,Mi,S])).	
